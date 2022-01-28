6a3a4dd982a8ecb50be41f99932300814234396f
Aaron Decker <adecker4@huskers.unl.edu>
2022-01-28 16:38:02 +0000

    added my majr to the output

---
+++ hello.c
@@ -0,0 +1,22 @@
+/**
+ * Author1: Jamie Rodriguez
+ * jrodriguez37@huskers.unl.edu
+ * 
+ * Author2: Aaron Decker
+ * adecker4@unl.edu
+ * 
+ * Date: 2022/01/28
+ *
+ * A program that says our names and majors in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Jamie Rodriguez and Aaron Decker!\n");
+  printf("Electrical Enginering and Computer Enginering\n");
+  
+  return 0;
+}
